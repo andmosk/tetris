7b998aba27a20ee5feef7cd918e997533d8c76ac
Andriy Moskaliuk <moskaliuk.ua@gmail.com.com>
2018-05-14 15:39:41 +0300

    Create Project, basic container, basic figure, add handling of key events

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: gray;
+  height: 80vh;
+  width: 80vh;
+  position: relative;
+`;
+
+const Square = styled.div`
+  width: 44px;
+  height: 44px;
+  background: black;
+  position: absolute;
+  border: 3px solid white;
+`;
+
+const Figure = ({position={x:0, y:0}, elements=[{x:0,y:0}], angle}) => {
+  const convertToPx = (pointPosition, figurePosition) => {
+    return 50 * (pointPosition + figurePosition)  + 'px';
+  }
+  const squares = elements.map((element, index) => {
+    return <Square key={index} style={{top: convertToPx(element.y, position.y), left: convertToPx(element.x, position.x)}} />
+  })
+  return <Fragment>
+    {squares}
+  </Fragment>
+}
+
+class App extends Component {
+  state = {
+    FigurePosition: {x:0, y:0}
+  }
+  handleMoveRight = () => {
+    console.warn('right');
+    const {x: prevX} =  this.state.FigurePosition;
+    this.setState({FigurePosition: {...this.state.FigurePosition, x: prevX + 1, }});
+  }
+  handleMoveLeft = () => {
+    console.warn('left');
+    const {x: prevX} =  this.state.FigurePosition;
+    this.setState({FigurePosition: {...this.state.FigurePosition, x: prevX - 1, }});
+  }
+  handleRotate = () => {
+    console.warn('rotate');
+  }
+  handleMoveDown = () => {
+    console.warn('down');    
+  }
+  handleKeyUp = (e) => {
+    
+    switch (e.which) {
+      case 65:
+      case 37:
+      this.handleMoveLeft()
+      break;
+      case 68:
+      case 39:
+      this.handleMoveRight()
+      break;
+      case 87:
+      case 38:
+      this.handleRotate()
+      break;
+      case 83:
+      case 40:
+      this.handleMoveDown()
+      break;
+    }
+  }
+
+  render() {
+    const {FigurePosition} = this.state;
+    return (
+      <Container onKeyUp={this.handleKeyUp} tabIndex="0">
+        <Figure elements={[{x:0, y:0}, {x:0, y: 1}]} position={FigurePosition} />
+      </Container>
+    );
+  }
+}
+
+export default App;
