25a5db5ff53f49d7597c7c3f083a686bc658a858
Andriy Moskaliuk <moskaliuk.ua@gmail.com>
2018-05-30 16:00:18 +0300

    Style Container

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: gray;
+  outline: none;
+  border: 10px solid #ddd;
+  background: #333;
   height: ${({ height }) => (height ? height + "px" : "100vh")};
   width: ${({ width }) => (width ? width + "px" : "100vw")};
   position: relative;
