0ee9677cfee5d0090c3ae36389368c8acba7df8f
Andriy Moskaliuk <moskaliuk.ua@gmail.com>
2018-05-30 16:00:56 +0300

    Change styles of Block

--- src/Block.js
+++ src/Block.js
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 
 export const Block = styled.div`
-  width: 44px;
-  height: 44px;
-  background: black;
+  box-sizing: border-box;
+  ${({ blockSize = 45 }) => {
+    return `
+     width: ${blockSize}px;
+     height: ${blockSize}px;
+    `;
+  }};
+  background: #555;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: absolute;
-  border: 3px solid white;
+  border: 3px solid #ddd;
+  box-shadow: 5px 5px 2px rgba(0, 0, 0, 0.25);
   top: ${({ top }) => (top ? top + "px" : 0 + "px")};
   left: ${({ left }) => (left ? left + "px" : 0 + "px")};
-  color: white;
+  color: #ddd;
 `;
