e9d3575b3e69534563897d53a875fb7ba39ae02e
Andriy Moskaliuk <moskaliuk.ua@gmail.com>
2018-05-30 19:58:29 +0300

    Add debounce

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,21 @@
 export const convert2px = (pointPosition, figurePosition = 0, squareSize) => {
   return squareSize * (pointPosition + figurePosition);
 };
-
+export function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 export function shuffle(array) {
   let counter = array.length;
 
